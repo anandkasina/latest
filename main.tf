3524217133f6dba4714aebc234f3f5eb7ff448d5
anandkasina <106368078+anandkasina@users.noreply.github.com>
2022-07-07 16:47:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,8 +31,9 @@ resource "aws_iam_policy" "policy" {
   name        = "test_policy"
   path        = "/"
   description = "My test policy"
+  arn         = "arn:aws:iam::aws:policy/IAMFullAccess"
 
-  # Terraform's "jsonencode" function converts a
+  # Terraform's jsonencode function converts a
   # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
     Version = "2012-10-17"
