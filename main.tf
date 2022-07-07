86cbe4ce677ecd1eb4060116afcdea1fcd026164
anandkasina <106368078+anandkasina@users.noreply.github.com>
2022-07-07 16:49:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,13 +27,12 @@ resource "aws_iam_role" "role" {
   }
 }
 
-resource "aws_iam_policy" "policy" {
+resource "aws_iam_policy" "policy1" {
   name        = "test_policy"
   path        = "/"
   description = "My test policy"
-  arn         = "arn:aws:iam::aws:policy/IAMFullAccess"
 
-  # Terraform's jsonencode function converts a
+  # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
     Version = "2012-10-17"
@@ -51,7 +50,7 @@ resource "aws_iam_policy" "policy" {
 
 resource "aws_iam_role_policy_attachment" "attachment" {
   role       = aws_iam_role.role.name
-  policy_arn = data.aws_iam_policy.policy.arn
+  policy_arn = data.aws_iam_policy.policy1.policy
 }
 
 resource "vault_aws_secret_backend" "aws" {
